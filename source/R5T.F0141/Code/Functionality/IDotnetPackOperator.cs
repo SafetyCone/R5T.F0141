92c073ae45060d24dac79e0e8493ae15f208c266
davidcoats <david@getsafetycone.com>
2023-07-27 01:31:01 -0700

    Interim changes.

---
+++ source/R5T.F0141/Code/Functionality/IDotnetPackOperator.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.T0132;
+using R5T.T0162;
+using R5T.T0212.F000;
+using R5T.T0215;
+using R5T.T0218;
+
+
+namespace R5T.F0141
+{
+    [FunctionalityMarker]
+    public partial interface IDotnetPackOperator : IFunctionalityMarker
+    {
+        public async Task<IDictionary<IIdentityName, MemberDocumentation>> Get_MemberDocumentationsByIdentityName(
+            IDotnetPackName dotnetPackName,
+            ITargetFrameworkMoniker targetFrameworkMoniker)
+        {
+            var documentationXmlFilePaths = Instances.DotnetPackPathOperator.GetDocumentationXmlFilePaths(
+                dotnetPackName,
+                targetFrameworkMoniker);
+
+            var documentationTarget = new DotnetFrameworkTarget()
+            {
+                TargetFrameworkMoniker = targetFrameworkMoniker,
+            };
+
+            var memberDocumentationsByIdentityName = await Instances.DocumentationFileOperator.Get_MemberDocumentationsByIdentityName(
+                documentationXmlFilePaths,
+                documentationTarget);
+
+            return memberDocumentationsByIdentityName;
+        }
+    }
+}
--- source/R5T.F0141/Code/Instances.cs
+++ source/R5T.F0141/Code/Instances.cs
@@ -5,6 +5,7 @@ namespace R5T.F0141
 {
     public static class Instances
     {
-		
+        public static T0212.F000.IDocumentationFileOperator DocumentationFileOperator => T0212.F000.DocumentationFileOperator.Instance;
+        public static F0138.IDotnetPackPathOperator DotnetPackPathOperator => F0138.DotnetPackPathOperator.Instance;
     }
 }
